afed3bc3dc0ce83e3bcc9b9fefb5c07c381cd7d0
Hamza1610 <muhammadhamza162003@gmail.com>
2023-12-15 16:23:45 -0800

    added BlocklyBoard component

---
+++ custom-blockly/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import BlocklyBoard from './BlocklyPad';
+
+function App() {
+  return (
+    <div className="App">
+      <BlocklyBoard />
+    </div>
+  );
+}
+
+export default App;
